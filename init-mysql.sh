1985ff0943bda113c093be2bc52aec4ea0970022
netroby <netroby@users.noreply.github.com>
2015-10-22 12:06:48 +0800

    Update initialize script

--- init-mysql.sh
+++ init-mysql.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-mysql_install_db --user=mysql --datadir=/mysql-data
-mysqld_safe &
+DATADIR="/mysql-data"
+echo 'Initializing database'
+mysqld --initialize-insecure=on --user=mysql --datadir="$DATADIR"
+echo 'Database initialized'
+mysqld --user=mysql --datadir="$DATADIR" &
 sleep 15
 echo "use mysql;delete from mysql.user where User = ''; update user set Host='%' where User='root' and Host='localhost';flush privileges;" | mysql -u root -h 127.0.0.1
 echo "Ok, MySQL now up and running"
