4f3286ee229ce1fd6779e1da8bda79738132861d
ZhiFeng Hu <hufeng1987@gmail.com>
2015-10-29 18:00:26 +0800

    Update docker create scripts
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,15 @@
-FROM fedora:22
-RUN dnf clean all && \
-    dnf install -y http://dev.mysql.com/get/mysql57-community-release-fc22-7.noarch.rpm && \
-    dnf update -y && \
-    dnf install -y iproute procps-ng htop mysql-community-server curl && \
-    dnf update -y && \
-    dnf clean all 
+FROM centos:7
+
+ENV PACKAGE_URL https://repo.mysql.com/yum/mysql-5.7-community/docker/x86_64/mysql-community-server-minimal-5.7.9-1.el7.x86_64.rpm
+
+RUN rpmkeys --import http://repo.mysql.com/RPM-GPG-KEY-mysql \
+  && yum install -y $PACKAGE_URL \
+  && rm -rf /var/cache/yum/*
 
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod a+x /entrypoint.sh
+
+VOLUME /var/lib/mysql
 
 COPY my.cnf /etc/my.cnf
 
@@ -20,3 +22,4 @@ EXPOSE 3306
 WORKDIR /root
 
 CMD ["mysqld", "-u" , "mysql"]
+
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,68 @@
-#!/bin/sh
+#!/bin/bash
+set -e
 
-set -e 
+# if command starts with an option, prepend mysqld
 if [ "${1:0:1}" = '-' ]; then
     set -- mysqld "$@"
 fi
-DATADIR="/mysql-data"
-if [ "$1" = 'mysqld' ]; then 
-    if [ ! -d $DATADIR/mysql ]; then
+
+if [ "$1" = 'mysqld' ]; then
+    # Get config
+    DATADIR="$("$@" --verbose --help --log-bin-index=/tmp/tmp.index 2>/dev/null | awk '$1 == "datadir" { print $2; exit }')"
+
+    if [ ! -d "$DATADIR/mysql" ]; then
+        mkdir -p "$DATADIR"
+        chown -R mysql:mysql "$DATADIR"
+
         echo 'Initializing database'
         mysqld --initialize-insecure=on --user=mysql --datadir="$DATADIR"
         echo 'Database initialized'
-        mysqld --user=mysql --datadir="$DATADIR" &
-        sleep 15
-        echo "use mysql;delete from mysql.user where User = ''; update user set Host='%' where User='root' and Host='localhost';flush privileges;" | mysql -u root -h 127.0.0.1
+
+        mysqld --user=mysql --datadir="$DATADIR" --skip-networking &
+        pid="$!"
+
+        mysql=( mysql --protocol=socket -uroot )
+
+        for i in {30..0}; do
+            if echo 'SELECT 1' | "${mysql[@]}" &> /dev/null; then
+                break
+            fi
+            echo 'MySQL init process in progress...'
+            sleep 1
+        done
+        if [ "$i" = 0 ]; then
+            echo >&2 'MySQL init process failed.'
+            exit 1
+        fi
+
+        mysql_tzinfo_to_sql /usr/share/zoneinfo | "${mysql[@]}" mysql
+
+        "${mysql[@]}" <<-EOSQL
+            -- What's done in this file shouldn't be replicated
+            --  or products like mysql-fabric won't work
+            SET @@SESSION.SQL_LOG_BIN=0;
+            DELETE FROM mysql.user ;
+            CREATE USER 'root'@'%' IDENTIFIED BY '' ;
+            GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;
+            DROP DATABASE IF EXISTS test ;
+            FLUSH PRIVILEGES ;
+        EOSQL
+
+
+        echo
+
+        if ! kill -s TERM "$pid" || ! wait "$pid"; then
+            echo >&2 'MySQL init process failed.'
+            exit 1
+        fi
+
+        echo
+        echo 'MySQL init process done. Ready for start up.'
+        echo
     fi
+
+    chown -R mysql:mysql "$DATADIR"
 fi
-chown -R mysql:mysql "$DATADIR"
-chmod -R a+rx /mysql-data
+
 exec "$@"
+
