f056320435e8dec77fbcbbc9e073384be92ee17c
netroby <netroby@users.noreply.github.com>
2015-10-29 10:52:02 +0800

    Clean entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 if [ "${1:0:1}" = '-' ]; then
     set -- mysqld "$@"
 fi
+DATADIR="/mysql-data"
 if [ "$1" = 'mysqld' ]; then 
-    DATADIR="/mysql-data"
     if [ ! -d $DATADIR/mysql ]; then
         echo 'Initializing database'
         mysqld --initialize-insecure=on --user=mysql --datadir="$DATADIR"
@@ -13,13 +13,8 @@ if [ "$1" = 'mysqld' ]; then
         mysqld --user=mysql --datadir="$DATADIR" &
         sleep 15
         echo "use mysql;delete from mysql.user where User = ''; update user set Host='%' where User='root' and Host='localhost';flush privileges;" | mysql -u root -h 127.0.0.1
-        chown -R mysql:mysql "$DATADIR"
-
-        /root/init-mysql.sh
-    else
-        chown -R mysql:mysql /mysql-data
-        chmod -R a+rx /mysql-data
     fi
 fi
-
+chown -R mysql:mysql "$DATADIR"
+chmod -R a+rx /mysql-data
 exec "$@"
